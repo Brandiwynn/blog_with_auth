e4a60da9116c07d7fbb6817f40ba171ea8456153
Brandiwynn <brandi@sinzinger.us>
2026-03-08 13:41:37 -0400

    updated 2026-03-08

--- .history/forms_20260308133755.py
+++ .history/forms_20260308133755.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
 
-# WTForm for creating a blog post
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
@@ -21,14 +20,7 @@ class RegisterForm(FlaskForm):
     submit = SubmitField("Sign Me Up!")
 
 
-# Create a form to login existing users
-class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Let Me In!")
+# TODO: Create a LoginForm to login existing users
 
 
-# Create a form to add comments
-class CommentForm(FlaskForm):
-    comment_text = CKEditorField("Comment", validators=[DataRequired()])
-    submit = SubmitField("Submit Comment")
+# TODO: Create a CommentForm so users can leave comments below posts
